4b02be460b58422f10f67cbe4e174e8c524722fd
Sajid-tech <80850448+Sajid-tech@users.noreply.github.com>
2023-09-29 16:23:24 +0530

    Ai summarizer

---
+++ src/components/Demo.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
+import { useLazyGetSummaryQuery } from "../services/article";
+
+const Demo = () => {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  // RTK lazy query
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handleSumbit = async (e) => {
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      setArticle(newArticle);
+    }
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search  */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center "
+          onSubmit={handleSumbit}
+        >
+          <img
+            src={linkIcon}
+            alt="linkIcon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700 "
+          >
+            <p>↵</p>
+          </button>
+        </form>
+        {/* Browse URL History  */}
+      </div>
+      {/* Display Result  */}
+    </section>
+  );
+};
+
+export default Demo;
